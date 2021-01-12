0e14c41e3eb2f4f90cc2e5b797197edda91636da
oYuu <61640871+oYuu@users.noreply.github.com>
2021-01-12 16:43:02 +0900

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by Yuichi Oyama on 2021/01/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
