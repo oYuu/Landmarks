4a01104904915664472fb8e0a7c9c5a787d60947
oYuu <61640871+oYuu@users.noreply.github.com>
2021-01-16 09:39:11 +0900

    :alembic: complete the tutorial on handling user input

--- SwiftUITutorial/SwiftUITutorialApp.swift
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct SwiftUITutorialApp: App {
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
